c098728332c86fedab71e6b9c6d83ceaedd23528
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2024-06-04 00:09:44 +0000

    import datetime module from the datetime library

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask("app")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///villain.db"
