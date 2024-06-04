65bb3e4e6dc59dfa8757c54f2e738337b84fb707
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2024-06-04 00:18:17 +0000

    Set up database columns with appropriate values

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ db = SQLAlchemy(app)
 
 class Villain(db.Model):
   id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(80), unique=True, nullable=False)
+  description = db.Column(db.String(250), nullable=False)
+  interests = db.Column(db.String(250), nullable=False)
+  url = db.Column(db.String(250), nullable=False)
+  date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 @app.route("/")
 def hello_world():
