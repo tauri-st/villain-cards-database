1a044d94eb44639b1a31e64fa772a0c00639a36a
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2024-06-05 22:27:31 +0000

    import requests library

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import requests
 
 app = Flask("app")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///villain.db"
