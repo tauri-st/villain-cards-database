e740f8d2257010864c6a3b360576d3d7a516e1e0
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2024-06-10 22:34:07 +0000

    Remove comment out of addVillain route and move deleteVillain route beneath it

--- main.py
+++ main.py
@@ -30,13 +30,40 @@ with app.app_context():
 def villains_cards():
   return render_template("villain.html", villains=Villain.query.all())
   
-#@app.route("/add", methods=["GET"])
-#def add_villain():
-  #return render_template("addvillain.html", errors=[])
+@app.route("/add", methods=["GET"])
+def add_villain():
+  return render_template("addvillain.html", errors=[])
 
 @app.route("/delete", methods=["GET"])
 def delete_villain():
   return render_template("deletevillain.html", errors=[])
+  
+@app.route("/addVillain", methods=["POST"])
+def add_user():
+  errors = []
+  name = request.form.get("name")
+  if not name:
+    errors.append("Oops! Looks like you forgot a name!")
+  description = request.form.get("description")
+  if not description:
+    errors.append("Oops! Looks like you forgot a description!")
+  interests = request.form.get("interests")
+  if not interests:
+    errors.append("Oops! Looks like you forgot some interests!")
+  url = request.form.get("url")
+  if not url:
+    errors.append("Oops! Looks like you forgot an image!")
+
+  villain = Villain.query.filter_by(name=name).first()
+  if villain:
+    errors.append("Oops! A villain with that name already exists!")
+  if errors:
+    return render_template("addvillain.html", errors=errors)
+  else:
+    new_villain = Villain(name=name,description=description, interests=interests, url=url)
+    db.session.add(new_villain)
+    db.session.commit()
+    return render_template("villain.html", villains=Villain.query.all())
 
 @app.route("/deleteVillain", methods=["POST"])
 def delete_user():
@@ -48,32 +75,5 @@ def delete_user():
     return render_template("villain.html", villains=Villain.query.all())
   else:
     return render_template("deletevillain.html", errors=["Oops! That villain doesn't exist!"])
-  
-#@app.route("/addVillain", methods=["POST"])
-#def add_user():
-#  errors = []
-#  name = request.form.get("name")
-#  if not name:
-#    errors.append("Oops! Looks like you forgot a name!")
-#  description = request.form.get("description")
-#  if not description:
-#    errors.append("Oops! Looks like you forgot a description!")
-#  interests = request.form.get("interests")
-#  if not interests:
-#    errors.append("Oops! Looks like you forgot some interests!")
-#  url = request.form.get("url")
-#  if not url:
-#    errors.append("Oops! Looks like you forgot an image!")
-
-#  villain = Villain.query.filter_by(name=name).first()
-#  if villain:
-#    errors.append("Oops! A villain with that name already exists!")
-#  if errors:
-#    return render_template("addvillain.html", errors=errors)
-#  else:
-#    new_villain = Villain(name=name,description=description, interests=interests, url=url)
-#    db.session.add(new_villain)
-#    db.session.commit()
-#    return render_template("villain.html", villains=Villain.query.all())
 
 app.run(host='0.0.0.0', port=8080)
